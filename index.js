077d9d11bffc50b0abe1c9aeccd7a04b93961ad9
loladmebrah <shernandezh@unal.edu.co>
2019-04-01 15:27:03 -0500

    first commit test

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+	res.send("SuccesS!");
+});
+
+const server = app.listen(process.env.PORT || 8080, ()=>{
+	const host = server.address().address;
+	const port = server.address().port;
+	console.log(`Example app listening at http://${host}:${port}`);
+});
